ff1c46982b13571cee7b0f1708824f9b9f5f609d
ElkanSrmnt <elkansarmiento205@gmail.com>
2026-04-24 10:57:34 +0800

    Refactor login system with new function names

--- Elkan Sarmiento/week09_file_handling/login_file_system.py
+++ Elkan Sarmiento/week09_file_handling/login_file_system.py
@@ -1,39 +1,39 @@
-def register_nbs():
-    username_nbs = input("Enter username: ")
-    password_nbs = input("Enter password: ")
-    with open("users.txt", "a") as file_nbs:
-        file_nbs.write(username_nbs + "," + password_nbs + "\n")
+def register_eas():
+    username_eas = input("Enter username: ")
+    password_eas = input("Enter password: ")
+    with open("users.txt", "a") as file_eas:
+        file_eas.write(username_eas + "," + password_eas + "\n")
     print("Registration successful!")
 
-def login_nbs():
-    username_nbs = input("Enter username: ")
-    password_nbs = input("Enter password: ")
+def login_eas():
+    username_eas = input("Enter username: ")
+    password_eas = input("Enter password: ")
     try:
         with open("users.txt", "r") as file_nbs:
-            for line_nbs in file_nbs:
-                stored_user_nbs, stored_pass_nbs = line_nbs.strip().split(",")
-                if username_nbs == stored_user_nbs and password_nbs == stored_pass_nbs:
+            for line_eas in file_nbs:
+                stored_user_eas, stored_pass_eas = line_eas.strip().split(",")
+                if username_eas == stored_user_eas and password_eas == stored_pass_eas:
                     print("Login successful!")
                     return
         print("Invalid credentials!")
     except FileNotFoundError:
         print("No users registered yet!")
 
-def main_nbs():
+def main_eas():
     while True:
         print("\n1. Register")
         print("2. Login")
         print("3. Exit")
-        choice_nbs = input("Enter choice: ")
+        choice_eas = input("Enter choice: ")
         
-        if choice_nbs == "1":
-            register_nbs()
-        elif choice_nbs == "2":
-            login_nbs()
-        elif choice_nbs == "3":
+        if choice_eas == "1":
+            register_eas()
+        elif choice_eas == "2":
+            login_eas()
+        elif choice_eas == "3":
             print("Goodbye!")
             break
         else:
             print("Invalid choice!")
 
-main_nbs()
+main_eas()
